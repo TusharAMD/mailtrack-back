db801ac7c2545cdadff8e3dda22639187219518e
Tushar <tusharvamdoskar#gmail.com>
2022-01-08 21:56:28 +0530

    1

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, send_from_directory,jsonify
+import socket
+from requests import get
+
+app = Flask(__name__, static_url_path='/static')
+
+
+@app.route('/explorer/<username>/<path>',methods=["GET","POST"])
+def explorer(username,path):
+    
+    ip = get('https://api.ipify.org').text
+    print ('My public IP address is:', ip)
+    print("Path Name:", path,"\nUserName:", username)
+    return send_from_directory('static', f"{username}/{path}")
+
+if __name__ == "__main__":
+    app.run(debug=True)
