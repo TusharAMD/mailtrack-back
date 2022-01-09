9dbf47624f30b7aafe2e474f9877211b0df9d135
Tushar <tusharvamdoskar#gmail.com>
2022-01-09 16:38:30 +0530

    1

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, request, send_from_directory,jsonify
 import socket
 from requests import get
-from flask_cors import CORS
+from flask_cors import CORS,cross_origin
 import smtplib, ssl
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import os
+from PIL import Image
+import datetime
 
 app = Flask(__name__, static_url_path='/static')
 cors = CORS(app)
@@ -23,46 +25,55 @@ def explorer(username,path):
 @app.route('/sendemail',methods=["GET","POST"]) 
 @cross_origin()   
 def sendemail():
-    sender_email = request.json["useremail"]
-    receiver_email = request.json["recemail"]
-    password = request.json["userpass"]
+    if request.method=="POST":
+        sender_email = request.json["useremail"]
+        receiver_email = request.json["recemail"]
+        password = request.json["userpass"]
 
-    message = MIMEMultipart("alternative")
-    message["Subject"] = request.json["subject"]
-    message["From"] = sender_email
-    message["To"] = receiver_email
-    
-    print(os.path.isdir(f"static\{request.json["useremail"]}") )
-    
-    # Create the plain-text and HTML version of your message
-    text = request.json["mailcontent"]
-    html = """\
-    <html>
-      <body>
-        <p>
-           <img src="https://mailtracktva.herokuapp.com/explorer/tva/myphoto.jpg"></img>
-        </p>
-      </body>
-    </html>
-    """
-    
-    # Turn these into plain/html MIMEText objects
-    part1 = MIMEText(text, "plain")
-    part2 = MIMEText(html, "html")
+        message = MIMEMultipart("alternative")
+        message["Subject"] = request.json["subject"]
+        message["From"] = sender_email
+        message["To"] = receiver_email
+        
+        if not (os.path.isdir(f"static/{request.json['useremail']}")):
+            os.mkdir(f"static/{request.json['useremail']}")
+        image = Image.new('RGB', (1, 1))
+        filename = str(sender_email)+"_"+str(receiver_email)+"_"+str(datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S"))
+        im1 = image.save(f"static/{request.json['useremail']}/{filename}.jpg")
+        
+        
+        # Create the plain-text and HTML version of your message
+        text = request.json["mailcontent"]
+        html = f"""\
+        <html>
+          <body>
+            <p>
+               <img src="https://mailtracktva.herokuapp.com/explorer/{request.json['useremail']}/{filename}.jpg"></img>
+            </p>
+          </body>
+        </html>
+        """
+        
+        print(f"https://mailtracktva.herokuapp.com/explorer/{request.json['useremail']}/{filename}.jpg")
+        
+        # Turn these into plain/html MIMEText objects
+        part1 = MIMEText(text, "plain")
+        part2 = MIMEText(html, "html")
 
-    # Add HTML/plain-text parts to MIMEMultipart message
-    # The email client will try to render the last part first
-    message.attach(part1)
-    message.attach(part2)
+        # Add HTML/plain-text parts to MIMEMultipart message
+        # The email client will try to render the last part first
+        message.attach(part1)
+        message.attach(part2)
 
-    # Create secure connection with server and send email
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-        server.login(sender_email, password)
-        server.sendmail(
-            sender_email, receiver_email, message.as_string()
-        )
-    print(request.json)
+        # Create secure connection with server and send email
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+            server.login(sender_email, password)
+            server.sendmail(
+                sender_email, receiver_email, message.as_string()
+            )
+        print(request.json)
+        return jsonify({1:1})
     return jsonify({1:1})
 if __name__ == "__main__":
     app.run(debug=True)
